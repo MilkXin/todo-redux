19468c149046c446641acc71571ec4503077f8b6
chenxin <het0121>
2020-04-28 16:52:51 +0800

    type设为常量引入，action提到单独文件

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import 'antd/dist/antd.css'
 import { Input, Button, List } from 'antd'
 import store from './store'
-
-
+import { changeInputAction, addItemAction, deleteItemAction } from './store/actionCreators'
 
 class TodoList extends Component {
     constructor(props) {
@@ -20,23 +19,17 @@ class TodoList extends Component {
     }
 
     changeInputValue = (e) => {
-        const action = {
-            type: 'changeInput',
-            value: e.target.value
-        }
+        const action = changeInputAction(e.target.value)
         store.dispatch(action)
     }
 
     clickBtn = () => {
-        const action = { type: 'addItem' }
+        const action = addItemAction()
         store.dispatch(action)
     }
 
     deleteItem = (index) => {
-        const action = {
-            type: 'deleteItem',
-            index
-        }
+        const action = deleteItemAction(index)
         store.dispatch(action)
     }
 
